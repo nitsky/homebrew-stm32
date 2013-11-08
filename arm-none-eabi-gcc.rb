2566bd4b285d9cc0549205f03f6329b1484db20d
David Yamnitsky <davidy@me.com>
2013-11-08 17:13:50 -0500

    initial version of formulae

---
+++ arm-none-eabi-gcc.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class ArmNoneEabiGcc < Formula
+
+  homepage 'https://launchpad.net/gcc-arm-embdded'
+  version '20130313'
+  url 'https://launchpadlibrarian.net/151487551/gcc-arm-none-eabi-4_7-2013q3-20130916-mac.tar.bz2'
+  sha1 'a29eda3d4351bfe47749a242f6faa7cbd630d28b'
+
+  def install
+    system 'cp', '-rv', 'arm-none-eabi', 'bin', 'lib', 'share', "#{prefix}/"
+  end
+
+end
---
+++ stlink.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Stlink < Formula
+
+    homepage 'https://github.com/texane/stlink'
+    head 'https://github.com/texane/stlink.git'
+
+    depends_on 'automake' => :build
+    depends_on 'libusb' => :build
+    depends_on 'pkg-config' => :build
+
+    def install
+        system "./autogen.sh"
+        system "./configure", "--prefix=#{prefix}"
+        system "make"
+        system "make install"
+    end
+
+end
