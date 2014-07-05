61d9c6478f4b396b772f53b2c7517e457141c361
Markus Herb <mail@markusherb.de>
2014-07-05 19:07:42 +0200

    Fix GCC ARM Embedded URL

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class ArmNoneEabiGcc < Formula
 
-  homepage 'https://launchpad.net/gcc-arm-embdded'
+  homepage 'https://launchpad.net/gcc-arm-embedded'
   version '20140314'
   url 'https://launchpadlibrarian.net/170926386/gcc-arm-none-eabi-4_8-2014q1-20140314-mac.tar.bz2'
   sha1 'b40c4127f641170f016b77ad423caf8dfd46faac'
