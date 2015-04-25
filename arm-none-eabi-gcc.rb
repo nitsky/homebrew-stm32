0b4f604ef0ed79d0521a38a706e9c96a5f0f114c
Markus Herb <mail@markusherb.de>
2015-04-25 13:26:35 +0200

    arm-none-eabi-gcc 20150306

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -3,9 +3,9 @@ require 'formula'
 class ArmNoneEabiGcc < Formula
 
   homepage 'https://launchpad.net/gcc-arm-embedded'
-  version '20140609'
-  url 'https://launchpad.net/gcc-arm-embedded/4.8/4.8-2014-q2-update/+download/gcc-arm-none-eabi-4_8-2014q2-20140609-mac.tar.bz2'
-  sha1 '751895b7c967dcc964b98c86f06198a3457a859e'
+  version '20150306'
+  url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q1-update/+download/gcc-arm-none-eabi-4_9-2015q1-20150306-mac.tar.bz2'
+  sha256 '049469f06c35d1a7c6d333236bc62ed340531c4377b4e16ce8c5aa8d134e8e8b'
   
   def install
     system 'cp', '-rv', 'arm-none-eabi', 'bin', 'lib', 'share', "#{prefix}/"
