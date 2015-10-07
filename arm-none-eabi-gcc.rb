10e02813a1a8776870f6db10e910ab7acb72b8fc
Nikolaus Wittenstein <nikolaus.wittenstein@gmail.com>
2015-10-07 12:49:04 -0400

    Fix `brew audit` warnings

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -1,14 +1,11 @@
-require 'formula'
-
 class ArmNoneEabiGcc < Formula
+  desc "GCC for embedded ARM processors"
+  homepage "https://launchpad.net/gcc-arm-embedded"
+  url "https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/gcc-arm-none-eabi-4_9-2015q3-20150921-mac.tar.bz2"
+  version "20150921"
+  sha256 "a6353db31face60c2091c2c84c902fc4d566decd1aa04884cd822c383d13c9fa"
 
-  homepage 'https://launchpad.net/gcc-arm-embedded'
-  version '20150921'
-  url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/gcc-arm-none-eabi-4_9-2015q3-20150921-mac.tar.bz2'
-  sha256 'a6353db31face60c2091c2c84c902fc4d566decd1aa04884cd822c383d13c9fa'
-  
   def install
-    system 'cp', '-rv', 'arm-none-eabi', 'bin', 'lib', 'share', "#{prefix}/"
+    cp_r ["arm-none-eabi", "bin", "lib", "share"], "#{prefix}/"
   end
-
 end
