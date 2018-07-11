52c064754c41e0520a523e6864b79c1073e1a43d
Dylan McKay <me@dylanmckay.io>
2018-07-11 14:33:42 +1200

    Update to June 2018
    Homepage has changed to one hosted by ARM

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -1,9 +1,9 @@
 class ArmNoneEabiGcc < Formula
   desc "GCC for embedded ARM processors"
-  homepage "https://launchpad.net/gcc-arm-embedded"
-  url "https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/gcc-arm-none-eabi-4_9-2015q3-20150921-mac.tar.bz2"
-  version "20150921"
-  sha256 "a6353db31face60c2091c2c84c902fc4d566decd1aa04884cd822c383d13c9fa"
+  homepage "https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads"
+  url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-mac.tar.bz2?revision=982ef8a4-1815-4651-9c44-6144c9d8b34b?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Mac%20OS%20X,7-2018-q2-update"
+  version "20180627"
+  sha256 "c1c4af5226d52bd1b688cf1bd78f60eeea53b19fb337ef1df4380d752ba88759"
 
   def install
     cp_r ["arm-none-eabi", "bin", "lib", "share"], "#{prefix}/"
